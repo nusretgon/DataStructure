1ccfd502d5185935235e0ac6d551e9665cfacdab
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-12 22:12:53 +0300

    Add files via upload

--- Java/Queue.java
+++ Java/Queue.java
@@ -1,58 +1,78 @@
-public class Queue {	// This ADT(Abstract data type)  is FIFO(First in First Out) mentality.
-	public class node{	// We add any data to the tail.And we delete from head.
-		int data;
+
+public class Queue {
+     
+	Eleman head,tail;
+	
+	public Queue() {
 		
-		node next;
-		node(int data){
-			this.data=data;
+		head = null;
+		tail = null;
+	}
+	
+	
+	public void enqueue(int data) {
+		
+		Eleman yeni = new Eleman(data);
+		
+		if(head == null) {	
+			head = yeni;
+			tail = yeni;
 		}
-		node tail=null;
-		node head=null;
-		public node Tail() {
-			node curr=new node(data);
-			while (curr.next!=null) {	//We are doing traversal to the end.
-				curr=curr.next;
-			}
-			return curr;
-		}
-		public int peek() { // it is used to get the element at the front of the queue without removing it
-			return head.data;
-		}
-		public boolean isEmpty() {
-			return head==null;
-		}
-		public void enqueue(int data) { 
-			node el=new node(data);
-			if (isEmpty()) {
-				head=el;
-				tail=el;
-			}
-			else {
-				tail.next=el;
-				tail=el;
-			}
-		}
-		public int dequeue() {
-			if (isEmpty()) {
-				return -1;
-			}
-			if (head.next==null) {
-				int Del=head.data;
-				head=null;
-				tail=null;
-				return Del;
-			}
-			else {
-				int Del=head.data;
-				head=head.next;
-				return Del;
-			}
-		}
-		public void clear() {
-			head=null;
-			tail=null;
+		
+		else {
+			tail.next = yeni;
+			tail = yeni;
 		}
 	}
 
+	
+	public int dequeue() {
+		
+		if(head == null) {
+			head = head.next;
+		}
+      
+		return 0;
 	}
 
+
+	public int front() {
+		if(head != null)
+		return head.data;
+		else
+			return Integer.MIN_VALUE;
+	}
+
+	
+	public boolean isEmpty() {
+
+		return head == null;
+	}
+
+
+	public void show() {
+
+		Eleman temp = head;
+		
+		while(temp != null)
+		{
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+
+	public int size() {
+		
+        Eleman temp = head;
+        int count = 0 ;
+		
+		while(temp != null)
+		{
+			count ++;
+			temp = temp.next;
+		}
+		return count;
+	}
+
+}
