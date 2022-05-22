2554532dbd47603a2ef23f56c29ea47ced4ac1f8
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-22 12:09:17 +0300

    Delete Stack_Interface.java

--- Java/Stack/Stack_Interface.java
+++
@@ -1,6 +0,0 @@
-public interface Stack_Interface<Integer> {
-	boolean isEmpty();		// Returning a boolean is stack empty or not.
-	void push(int item);	// Adding item to top.
-	int pop();				// We are deleting top and returning top.
-	int top();				// We are returning top.
-}
