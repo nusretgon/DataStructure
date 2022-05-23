b0ca1c8b5386fd805370eaf979a5fc2d5a4fac42
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-05-24 00:13:03 +0300

    Add files via upload

---
+++ Java/Queue/Queue_Interface.java
@@ -0,0 +1,8 @@
+public interface Queue_Interface {
+	public void enqueue(int data);
+	public boolean isEmpty();
+	public int peek();
+	public node Tail();
+	public int dequeue();
+	public void clear();
+}
